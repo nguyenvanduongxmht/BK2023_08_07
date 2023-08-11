8f3f963db5e4593e4065d811c5fdae19106e384c
students <nguyenvanduong.xmht@gmail.com>
2023-08-11 09:35:10 +0700

    ok

---
+++ mvc/DotNetCoreFirstApplication/Controllers/NameController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DotNetCoreFirstApplication.Models;
+
+namespace DotNetCoreFirstApplication.Controllers;
+
+public class NameController : Controller
+{
+    private readonly ILogger<NameController> _logger;
+
+    public NameController(ILogger<NameController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpGet]
+    public IActionResult GetName()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult GetName(string abc)
+    {
+         ViewBag.SomeProperty =abc;
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
